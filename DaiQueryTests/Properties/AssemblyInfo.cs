6223c96b5cb8732c4fb0dfbed8011ca0f8eaaf41
m.garassini <mattia.garassini@sedapta.com>
2017-07-12 14:16:45 +0200

    Updated assembly info.

--- DaiQueryTests/Properties/AssemblyInfo.cs
+++ DaiQueryTests/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DaiQueryTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DaiQueryTests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
